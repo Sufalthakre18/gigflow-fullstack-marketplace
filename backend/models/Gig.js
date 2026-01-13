d80aae0010e6dd73c2300a266fb2aa668f03dec7
Sufal Thakre <sufalthakre1@gmail.com>
2026-01-13 18:11:30 +0530

    add models for Bid, Gig, and User

---
+++ backend/models/Gig.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const gigSchema=new mongoose.Schema({
+    title:String,
+    description:String,
+    budget:Number,
+    ownerId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    status:{type:String,default:'open'}
+})
+
+export default mongoose.model("Gig",gigSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-
-import dotenv from 'dotenv'
-dotenv.config()
-
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import connectDb from './config/db.js'
 
+
+dotenv.config()
 connectDb();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.get('/',(_,res)=>{
